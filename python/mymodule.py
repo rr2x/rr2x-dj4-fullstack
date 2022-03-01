6412804c60deb42f78ff8fdb098b44e4d5385994
rr2x <79620740+rr2x@users.noreply.github.com>
2022-03-01 11:47:57 +0800

    update

---
+++ python/mymodule.py
@@ -0,0 +1,10 @@
+def useful_func():
+    print("using useful_func()")
+
+
+class UsefulClass():
+    def __init__(self, message):
+        self.message = message
+
+    def report(self):
+        print(self.message)
