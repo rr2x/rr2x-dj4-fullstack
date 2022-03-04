58c9ce11b75ef68c45312a38b72727dd555ef6a3
rr2x <79620740+rr2x@users.noreply.github.com>
2022-03-04 16:57:59 +0800

    dj3

--- dj3/my_site/my_app/urls.py
+++ dj3/my_site/my_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "my_app"  # used in {% url '...'%}
+
 urlpatterns = [
-    path('', views.example_view, name="example_view")
+    path('', views.example_view, name="example_view"),
+    path('variable/', views.variable_view, name="variable_view"),
 ]
