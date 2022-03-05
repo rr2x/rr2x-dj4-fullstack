1727c0a9b9470045abb1f1c9f38147272dc0b3f0
rr2x <79620740+rr2x@users.noreply.github.com>
2022-03-05 15:21:37 +0800

    dj4

--- dj4/my_site/office/models.py
+++ dj4/my_site/office/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator # built-in validators
 
 
 class Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    age = models.IntegerField()
+    age = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(200)])
+    heartrate = models.IntegerField(default=60, validators=[MinValueValidator(1), MaxValueValidator(300)])
 
     def __str__(self):
         return f"{self.last_name},{self.first_name} is {self.age} years old"
